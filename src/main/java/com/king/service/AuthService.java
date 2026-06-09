1b2f19c813fd343a7c7337c5db0d3e8bc811d652
xyxy778-code <1605582452@qq.com>
2026-06-09 14:54:40 +0800

    auto commit 2026-06-09 14:54:40

---
+++ src/main/java/com/king/service/AuthService.java
@@ -0,0 +1,41 @@
+package com.king.service;
+
+import com.king.dao.DataStore;
+import com.king.entity.Player;
+import java.util.Optional;
+
+public class AuthService {
+    private final DataStore store = DataStore.getInstance();
+    private Player currentPlayer;
+
+    public Optional<Player> login(String username, String password) {
+        for (Player p : store.players.values()) {
+            if (p.getUsername().equals(username) && p.getPassword().equals(password)) {
+                currentPlayer = p;
+                return Optional.of(p);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public void logout() {
+        currentPlayer = null;
+    }
+
+    public boolean isLoggedIn() {
+        return currentPlayer != null;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public boolean register(String id, String username, String password, String nickname) {
+        for (Player p : store.players.values()) {
+            if (p.getUsername().equals(username)) return false;
+        }
+        Player player = new Player(id, username, password, nickname);
+        store.players.put(id, player);
+        return true;
+    }
+}
