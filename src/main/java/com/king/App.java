26be1f8412260e385152b70f83f3292934c8efbf
xyxy778-code <1605582452@qq.com>
2026-06-09 15:23:14 +0800

    auto commit 2026-06-09 15:23:14

--- src/main/java/com/king/App.java
+++ src/main/java/com/king/App.java
@@ -2,6 +2,10 @@ package com.king;
 
 import com.king.controller.MenuController;
 
+/**
+ * 王者荣耀管理系统 - 程序入口
+ * 启动控制台菜单交互界面
+ */
 public class App {
     public static void main(String[] args) {
         new MenuController().start();
